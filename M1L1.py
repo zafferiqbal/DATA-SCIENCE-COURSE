df3ae01c2b2803033937403fd8e7f4831e376337
zafferiqbal <101118051+zafferiqbal@users.noreply.github.com>
2026-07-30 22:32:51 +0530

    Add files via upload

---
+++ M1L1.py
@@ -0,0 +1,8 @@
+print("Hello, world!")
+
+print("My name is Zaffer Iqbal.")
+
+print("I love playing Football.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
